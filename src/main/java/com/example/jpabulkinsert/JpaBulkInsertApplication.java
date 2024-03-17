922c534a1ab1fc48a0101e279e42a4806cec7821
baker.lee <ljw722007@gmail.com>
2024-03-17 14:22:51 +0900

    init

---
+++ src/main/java/com/example/jpabulkinsert/JpaBulkInsertApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpabulkinsert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaBulkInsertApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaBulkInsertApplication.class, args);
+    }
+
+}
